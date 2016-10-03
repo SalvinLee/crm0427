9235d3648cfd5a235422dff5c28ef9a7b67b4a18
SalvinLee <287868178@qq.com>
2016-10-04 00:09:45 +0800

    ooo

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -9,7 +9,7 @@ package com.atguigu.github.test;
 public class Test01 {
 
 	public static void main(String[] args) {
-		System.out.println("hello 0427");
+		System.out.println("hello 0427.");
 		System.out.println("hello github 服务器");
 		System.out.println("hello salvinlee");
 		System.out.println("hello github 服务器2");
